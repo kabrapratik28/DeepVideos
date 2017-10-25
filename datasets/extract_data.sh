2bccfee5e8ca95ddd1c627834501d74b516e3080
vatsalgit <vatsalsh@usc.edu>
2017-10-24 23:49:14 -0700

    ASLAN data deprecated due to integrity issues with skvideo

--- datasets/extract_data.sh
+++ datasets/extract_data.sh
@@ -2,10 +2,10 @@ mkdir data
 cd data
 wget -r http://crcv.ucf.edu/data/UCF101/UCF101.rar
 sudo apt-get update
-sudo apt-get unrar
+sudo apt-get install unrar
 unrar e UCF101.rar
-wget http://www.cslab.openu.ac.il/personal/Hassner/aslan/Data/ASLAN_AVI.zip --user=aslan --password=aslan789
-sudo apt-get install unzip
-mkdir ASLAN
-unzip ASLAN_AVI.zip -d ASLAN
+#wget http://www.cslab.openu.ac.il/personal/Hassner/aslan/Data/ASLAN_AVI.zip --user=aslan --password=aslan789
+#sudo apt-get install unzip
+#mkdir ASLAN
+#unzip ASLAN_AVI.zip -d ASLAN
 
