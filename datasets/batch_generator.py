1c759ab80739857a2d432942ae93b3448d755787
vatsalgit <vatsalsh@usc.edu>
2017-11-09 05:29:34 -0800

    Clean up train test logic. Added randomization at training, batch iteration tests on validation. Fixed size validation and test sets.Category wise splitting

--- datasets/batch_generator.py
+++ datasets/batch_generator.py
@@ -4,13 +4,13 @@ from frame_extraction import frame_extractor
 import cPickle
 
 class datasets(object):
-    def __init__(self, batch_size=64, val_split=0.005, test_split=0.005, height=64, width=64, DIR='../../data', output_filename='../../all_videos.txt', ):
+    def __init__(self, batch_size=32, val_split=0.05, test_split=0.05, height=64, width=64, DIR='../../data', output_filename='../../all_videos.txt', interval=1,):
         self.file_path = os.path.abspath(os.path.dirname(__file__))
         self.DIR = os.path.join(self.file_path,DIR)
         self.output_filename = os.path.join(self.file_path,output_filename)
         self.batch_size = batch_size
-        self.flagged_activities = ['PlayingDaf', 'BodyWeightSquats', 'Nunchucks', 'ShavingBeard', 'SkyDiving']
         self.data = None
+        self.interval = interval
         self.frame_ext = frame_extractor(height=height,width=width)
         self.videos_to_text_file()
         self.load_problematic_videos()
@@ -32,38 +32,36 @@ class datasets(object):
                for filename in files:
                  f = os.path.join(path, filename)
                  a.write(str(f) + os.linesep)
-
+        self.categories = set(os.listdir(self.DIR))
 
     def train_test_split(self, split_test_data, split_validation_data):
         """
         split_test_data : '%' of test data to split between 0 to 1
         """
-        data = {}
-        unseen = []
-        seen = []
+        all_vids = []
         for line in open(self.output_filename):
             line = line.rstrip('\n')
             if line in self.blacklist:
                 continue
-            if any(substring in line for substring in self.flagged_activities):
-                unseen.append(line)
             else:
-                seen.append(line)
+                all_vids.append(line)
 
-        datasize = len(seen)
+        train = []
+        validation = []
+        test = []
 
-        #Random Shuffle
-        random.shuffle(seen)
+        for category in self.categories:
+            all_data_in_category = [path for path in all_vids if category in path]
+            datasize = len(all_data_in_category)
+            validation_index = int(datasize * split_validation_data)
+            validation.extend(all_data_in_category[:validation_index])
+            all_data_in_category = all_data_in_category[validation_index:]
+            test_index = int(datasize * split_test_data)
+            train.extend(all_data_in_category[test_index:])
+            test.extend(all_data_in_category[:test_index])
 
-        validation_index = int(datasize * split_validation_data)
-        data['validation'] = seen[:validation_index]
-
-        seen = seen[validation_index:]
-        test_index = int(datasize * split_test_data)
-        data['train'] = seen[test_index:]
-        data['test'] = seen[:test_index]
-        data['unseen'] = unseen
-
+        random.shuffle(train)
+        data = {'train':train, 'validation':validation[:50], 'test':test[:500]}
         self.data = data
 
     def train_next_batch(self,):
@@ -79,16 +77,18 @@ class datasets(object):
                 entry = None
                 try:
                     entry = train_iter.next()
-                except StopIteration:
+                except:
                     # Shuffle data for next rollover ...
                     random.shuffle(self.data['train'])
                     train_iter = iter(self.data['train'])
                 if entry != None:
                     curr_batch.append(entry)
             if curr_batch:
-                yield self.frame_ext.get_frames(curr_batch)
+                yield self.frame_ext.get_frames_with_interval_x(curr_batch, x= self.interval)
 
     def fixed_next_batch(self,data_iter):
+        assert self.batch_size < 50
+
         is_done = False
         while True:
             curr_batch = []
@@ -96,31 +96,20 @@ class datasets(object):
                 entry = None
                 try:
                     entry = data_iter.next()
-                except StopIteration:
+                except:
                     is_done = True
                     break
                 if entry != None:
                     curr_batch.append(entry)
             if len(curr_batch)==self.batch_size:
-                yield self.frame_ext.get_frames(curr_batch)
+                yield self.frame_ext.get_frames_with_interval_x(curr_batch, x= self.interval, randomize=False)
             if is_done:
                 break
 
     def val_next_batch(self,):
-        """
-        Returns lists of length batch_size.
-        This is a generator function, and it returns lists of the
-        entries from the supplied iterator.  Each list will have
-        batch_size entries, although the final list may be shorter.
-        """
         val_iter = iter(self.data['validation'])
         return self.fixed_next_batch(val_iter)
 
     def test_next_batch(self,):
-        """Returns lists of length batch_size.
-        This is a generator function, and it returns lists of the
-        entries from the supplied iterator.  Each list will have
-        batch_size entries, although the final list may be shorter.
-        """
         val_iter = iter(self.data['test'])
         return self.fixed_next_batch(val_iter)
