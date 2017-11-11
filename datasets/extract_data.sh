39c92336fef2413c9c65724ae9da8014c90b3d1e
Vatsal Shah <vatsalsh@usc.edu>
2017-11-10 23:26:48 -0800

    Updated shell script

--- datasets/extract_data.sh
+++ datasets/extract_data.sh
@@ -3,7 +3,7 @@ cd data
 wget -r http://crcv.ucf.edu/data/UCF101/UCF101.rar
 sudo apt-get update
 sudo apt-get install unrar
-unrar e UCF101.rar
+unrar x UCF101.rar
 #wget http://www.cslab.openu.ac.il/personal/Hassner/aslan/Data/ASLAN_AVI.zip --user=aslan --password=aslan789
 #sudo apt-get install unzip
 #mkdir ASLAN
